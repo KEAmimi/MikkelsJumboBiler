f8e5853807e8553d5c2065203435081d5ae7a030
KEAmimi <mimi0001@stud.kea.dk>
2025-03-18 10:39:01 +0100

    Image fix

--- src/main/java/org/example/springtest/controller/Controller.java
+++ src/main/java/org/example/springtest/controller/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
-
+//hallo
 @org.springframework.stereotype.Controller
 public class Controller {
 
